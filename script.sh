a4c6dc61d68b3f9b0cbb66146242d8fe8acdc17f
Anastasia <anastasia.vecherenko@yandex.ru>
2023-12-26 13:52:05 +0300

    Prov

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if grep -q "#" "$1"; then
-	echo "File contains simbol #"
+if grep -q "%" "$1"; then
+	echo "File contains simbol %  $1"
 	exit 1
 else
-	echo "File doesn't contain symbol #"
+	echo "File doesn't contain symbol %  $1"
 	exit 0
 fi
